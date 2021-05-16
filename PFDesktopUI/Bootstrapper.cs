5fef7b1d9a6f1d7c7bd870d06ab17464be9407bf
Michael Persson <mapersson@alumni.uwaterloo.ca>
2021-05-16 15:01:20 -0600

    Add Login Form
    Added login form to UI. Had to add PasswordBoxHelper to use PasswordBox with Calburn Micro. Also downgraded Calburn Micro to 3.2 to align with tutorial.

--- PFDesktopUI/Bootstrapper.cs
+++ PFDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using PFDesktopUI.Helpers;
 using PFDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace PFDesktopUI
 {
@@ -17,6 +19,10 @@ namespace PFDesktopUI
         public Bootstrapper()
         {
             Initialize();
+            ConventionManager.AddElementConvention<PasswordBox>(
+           PasswordBoxHelper.BoundPasswordProperty,
+           "Password",
+           "PasswordChanged");
         }
 
         protected override void Configure()
