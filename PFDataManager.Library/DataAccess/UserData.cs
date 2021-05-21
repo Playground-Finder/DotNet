1f16334bb4291b861ef081441ef353b1ac790885
Michael Persson <mapersson@alumni.uwaterloo.ca>
2021-05-20 20:29:38 -0600

    Add MongoDB Interface
    Added mongoDB client and supporting infrastructure to DataManager layer.
    Implementation is rough
    
    Update Web.config
    
    Edited out sensitive information
    
    Update Web.Config
    
    Removed connection string details from web.config and extracted them to a separate file that will not be included in source control. This is to protect database connections.
    Gitignore also updated to exclude the connection file.

---
+++ PFDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using PFDataManager.Library.Internal.DataAccess;
+using PFDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public UserModel GetUserById(string id)
+        {
+            MongoDataAccess client = new MongoDataAccess();
+            var database = client.GetDatabase("test");
+            UserModel user = database.GetCollection<UserModel>("users").FindSync(Builders<UserModel>.Filter.Eq("_id", id)).First();
+            return user;
+        }
+    }
+}
---
+++ PFDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using PFDataManager.Library.DataAccess;
+using PFDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PFDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        public UserModel GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+
+        }
+    }
+}
