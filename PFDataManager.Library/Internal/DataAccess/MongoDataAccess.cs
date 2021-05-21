10da43fd72048e899101198868caf8a82a48e520
Michael Persson <mapersson@alumni.uwaterloo.ca>
2021-05-20 20:29:38 -0600

    Add MongoDB Interface
    Added mongoDB client and supporting infrastructure to DataManager layer.
    Implementation is rough

---
+++ PFDataManager.Library/Internal/DataAccess/MongoDataAccess.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFDataManager.Library.Internal.DataAccess
+{
+    //TODO: May need to clean up before closing the application. 
+    //TODO: Investigate DI to allow for Unit Testing. 
+    //TODO: Set up as a singleton so only a single client exists. 
+    internal  class MongoDataAccess
+    {
+        private MongoClient _client;
+
+        public MongoDataAccess()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            if (_client == null)
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["mongo"].ConnectionString;
+                _client = new MongoClient(connectionString);
+            }
+        }
+
+        public IMongoDatabase GetDatabase(string name)
+        {
+            var database = _client.GetDatabase(name);
+            return database;
+        }
+
+
+    }
+}
