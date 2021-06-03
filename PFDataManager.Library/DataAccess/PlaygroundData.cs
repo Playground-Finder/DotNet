12155a1ae3b3afdf2ff9b9706bc4bc0d685dbd24
Michael Persson <62900241+mapersson@users.noreply.github.com>
2021-06-02 20:09:36 -0600

    Add Playground Queries
    Queries for all the individual filter options in the UI, plus the ability to filter by any combination of filter options.

--- PFDataManager.Library/DataAccess/PlaygroundData.cs
+++ PFDataManager.Library/DataAccess/PlaygroundData.cs
@@ -19,5 +19,94 @@ namespace PFDataManager.Library.DataAccess
             List<PlaygroundModel> playgrounds = database.GetCollection<PlaygroundModel>("playgrounds").FindSync(FilterDefinition<PlaygroundModel>.Empty).ToList();
             return playgrounds;
         }
+
+        public async Task<List<PlaygroundModel>> FilterPlaygroundsByAgeAsync(string age)
+        {
+            //TODO: Change to DI
+            MongoDataAccess client = new MongoDataAccess();
+            var database = client.GetDatabase("test");
+
+            var filter = Builders<PlaygroundModel>.Filter.Eq("age",age);
+
+            List<PlaygroundModel> playgrounds = await database.GetCollection<PlaygroundModel>("playgrounds").Find(filter).ToListAsync();
+
+            return playgrounds;
+        }
+
+        public async Task<List<PlaygroundModel>> FilterPlaygroundsByQuadrantAsync(string quadrant)
+        {
+            //TODO: Change to DI
+            MongoDataAccess client = new MongoDataAccess();
+            var database = client.GetDatabase("test");
+
+            var filter = Builders<PlaygroundModel>.Filter.Eq("quadrant", quadrant);
+
+            List<PlaygroundModel> playgrounds = await database.GetCollection<PlaygroundModel>("playgrounds").Find(filter).ToListAsync();
+
+            return playgrounds;
+
+        }
+
+        public async Task<List<PlaygroundModel>> FilterPlaygroundsByGroundCoverAsync(string groundCover)
+        {
+            //TODO: Change to DI
+            MongoDataAccess client = new MongoDataAccess();
+            var database = client.GetDatabase("test");
+
+            var filter = Builders<PlaygroundModel>.Filter.Eq("ground_cover", groundCover);
+
+            List<PlaygroundModel> playgrounds = await database.GetCollection<PlaygroundModel>("playgrounds").Find(filter).ToListAsync();
+
+            return playgrounds;
+        }
+
+        public async Task<List<PlaygroundModel>> FilterPlaygroundsBySizeAsync(string size)
+        {
+            //TODO: Change to DI
+            MongoDataAccess client = new MongoDataAccess();
+            var database = client.GetDatabase("test");
+
+            var filter = Builders<PlaygroundModel>.Filter.Eq("size", size);
+
+            List<PlaygroundModel> playgrounds = await database.GetCollection<PlaygroundModel>("playgrounds").Find(filter).ToListAsync();
+
+            return playgrounds;
+        }
+
+
+        public async Task<List<PlaygroundModel>> FilterPlaygroundsByAccessibilityAsync(string accessibility)
+        {
+            //TODO: Change to DI
+            MongoDataAccess client = new MongoDataAccess();
+            var database = client.GetDatabase("test");
+
+            var filter = Builders<PlaygroundModel>.Filter.Eq("accessibility", accessibility);
+
+            List<PlaygroundModel> playgrounds = await database.GetCollection<PlaygroundModel>("playgrounds").Find(filter).ToListAsync();
+
+            return playgrounds;
+        }
+
+        public async Task<List<PlaygroundModel>> FilterPlaygroundsByMultipleAsync(IDictionary<string,string> criteria)
+        {
+            //TODO: Change to DI
+            MongoDataAccess client = new MongoDataAccess();
+            var database = client.GetDatabase("test");
+
+            List<FilterDefinition<PlaygroundModel>> filterList = new List<FilterDefinition<PlaygroundModel>>();
+
+            foreach (var pair in criteria)
+            {
+                var stringField = new StringFieldDefinition<PlaygroundModel, string>(pair.Key);
+                var filter = Builders<PlaygroundModel>.Filter.Eq(stringField, pair.Value);
+                filterList.Add(filter);
+            }
+
+            var compiledFilters = Builders<PlaygroundModel>.Filter.And(filterList);
+
+            List<PlaygroundModel> playgrounds = await database.GetCollection<PlaygroundModel>("playgrounds").Find(compiledFilters).ToListAsync();
+
+            return playgrounds;
+        }
     }
 }
--- PFDataManager/Controllers/PlaygroundController.cs
+++ PFDataManager/Controllers/PlaygroundController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace PFDataManager.Controllers
@@ -12,6 +13,7 @@ namespace PFDataManager.Controllers
     /// <summary>
     /// Controller for accessing playground information.
     /// </summary>
+    [Authorize]
     [RoutePrefix("api/playground")]
     public class PlaygroundController : ApiController
     {
@@ -25,5 +27,89 @@ namespace PFDataManager.Controllers
             PlaygroundData data = new PlaygroundData();
             return data.GetAll();
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="age"></param>
+        /// <returns></returns>
+        [Route("FilterByAge")]
+        [HttpGet]
+        public async Task<List<PlaygroundModel>> FilterByAgeAsync(string age)
+        {
+            PlaygroundData data = new PlaygroundData();
+            return await data.FilterPlaygroundsByAgeAsync(age);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="quadrant"></param>
+        /// <returns></returns>
+        [Route("FilterByQuadrant")]
+        [HttpGet]
+        public async Task<List<PlaygroundModel>> FilterByQuadrantAsync(string quadrant)
+        {
+            PlaygroundData data = new PlaygroundData();
+            return await data.FilterPlaygroundsByQuadrantAsync(quadrant);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="groundCover"></param>
+        /// <returns></returns>
+        [Route("FilterByGroundCover")]
+        [HttpGet]
+        public async Task<List<PlaygroundModel>> FilterByGroundCoverAsync(string groundCover)
+        {
+            PlaygroundData data = new PlaygroundData();
+            return await data.FilterPlaygroundsByGroundCoverAsync(groundCover);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        [Route("FilterBySize")]
+        [HttpGet]
+        public async Task<List<PlaygroundModel>> FilterBySizeAsync(string size)
+        {
+            PlaygroundData data = new PlaygroundData();
+            return await data.FilterPlaygroundsBySizeAsync(size);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="accessibility"></param>
+        /// <returns></returns>
+        [Route("FilterByAccessibility")]
+        [HttpGet]
+        public async Task<List<PlaygroundModel>> FilterByAccessibilityAsync(string accessibility)
+        {
+            PlaygroundData data = new PlaygroundData();
+            return await data.FilterPlaygroundsByAccessibilityAsync(accessibility);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filters"></param>
+        /// <returns></returns>
+        [Route("FilterByMultiple")]
+        [HttpPost]
+        public async Task<List<PlaygroundModel>> FilterByMultiple([FromBody] IDictionary<string,string> filters)
+        {
+            
+            PlaygroundData data = new PlaygroundData();
+            return await data.FilterPlaygroundsByMultipleAsync(filters);
+        }
+
+
     }
+
+    
 }
